6cdbfd1b6091a44e46cb2f88e1c7f1c795e004ae
SAY10s <nikodemmajzner@gmail.com>
2024-05-13 19:59:21 +0200

    ADD: simple emiters

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { isBooleanObject } = require("util/types");
 const app = express();
 const serv = require("http").Server(app);
 app.get("/", (req, res) => {
@@ -7,3 +8,16 @@ app.get("/", (req, res) => {
 app.use("/client", express.static(__dirname + "/client"));
 serv.listen(2000);
 console.log("Server started.");
+
+const io = require("socket.io")(serv, {});
+io.sockets.on("connection", (socket) => {
+  console.log("socket connection");
+
+  socket.on("heyyo", () => {
+    console.log("YO BROSKIE");
+  });
+
+  socket.on("disconnect", () => {
+    console.log("socket disconnected");
+  });
+});
