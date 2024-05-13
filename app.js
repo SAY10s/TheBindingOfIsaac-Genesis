22447d41b7e050fd5879684897b507af1e2f8288
SAY10s <nikodemmajzner@gmail.com>
2024-05-13 20:14:37 +0200

    ADD: multiple players

--- app.js
+++ app.js
@@ -9,15 +9,35 @@ app.use("/client", express.static(__dirname + "/client"));
 serv.listen(2000);
 console.log("Server started.");
 
+const SOCKET_LIST = {};
+
 const io = require("socket.io")(serv, {});
 io.sockets.on("connection", (socket) => {
-  console.log("socket connection");
-
-  socket.on("heyyo", () => {
-    console.log("YO BROSKIE");
-  });
+  socket.id = Math.random();
+  socket.x = 0;
+  socket.y = 0;
+  socket.number = "" + Math.floor(10 * Math.random());
+  SOCKET_LIST[socket.id] = socket;
 
   socket.on("disconnect", () => {
-    console.log("socket disconnected");
+    delete SOCKET_LIST[socket.id];
   });
 });
+
+setInterval(() => {
+  const pack = [];
+  for (let i in SOCKET_LIST) {
+    let socket = SOCKET_LIST[i];
+    socket.x++;
+    socket.y++;
+    pack.push({
+      x: socket.x,
+      y: socket.y,
+      number: socket.number,
+    });
+  }
+  for (let i in SOCKET_LIST) {
+    let socket = SOCKET_LIST[i];
+    socket.emit("newPositions", pack);
+  }
+}, 1000 / 25);
