48be1a07b95987c33efc4ee79c07e5fbfc361783
SAY10s <nikodemmajzner@gmail.com>
2024-05-13 20:54:48 +0200

    Code cleanup

--- app.js
+++ app.js
@@ -10,49 +10,69 @@ serv.listen(2000);
 console.log("Server started.");
 
 const SOCKET_LIST = {};
-const PLAYER_LIST = {};
 
-const Player = (id) => {
+const Entity = () => {
   const self = {
     x: 250,
     y: 250,
-    id: id,
-    number: "" + Math.floor(10 * Math.random()),
-    pressingRight: false,
-    pressingLeft: false,
-    pressingUp: false,
-    pressingDown: false,
-    maxSpd: 10,
+    spdX: 0,
+    spdY: 0,
+    id: "",
+  };
+  self.update = () => {
+    self.updatePosition();
   };
   self.updatePosition = () => {
-    if (self.pressingRight) {
-      self.x += self.maxSpd;
-    }
-    if (self.pressingLeft) {
-      self.x -= self.maxSpd;
-    }
-    if (self.pressingUp) {
-      self.y -= self.maxSpd;
-    }
-    if (self.pressingDown) {
-      self.y += self.maxSpd;
-    }
+    self.x += self.spdX;
+    self.y += self.spdY;
   };
   return self;
 };
 
-const io = require("socket.io")(serv, {});
-io.sockets.on("connection", (socket) => {
-  socket.id = Math.random();
-  SOCKET_LIST[socket.id] = socket;
-  const player = Player(socket.id);
-  PLAYER_LIST[socket.id] = player;
+const Player = (id) => {
+  const self = Entity();
+  self.id = id;
+  self.number = "" + Math.floor(10 * Math.random());
+  self.pressingRight = false;
+  self.pressingLeft = false;
+  self.pressingUp = false;
+  self.pressingDown = false;
+  self.maxSpd = 10;
 
-  socket.on("disconnect", () => {
-    delete SOCKET_LIST[socket.id];
-    delete PLAYER_LIST[socket.id];
-  });
+  const super_update = self.update;
+  self.update = () => {
+    self.updateSpeed();
+    super_update();
+  };
+
+  self.updateSpeed = () => {
+    if (self.pressingRight) {
+      self.spdX = self.maxSpd;
+    } else if (self.pressingLeft) {
+      self.spdX = -self.maxSpd;
+    } else {
+      self.spdX = 0;
+    }
+
+    if (self.pressingUp) {
+      self.spdY = -self.maxSpd;
+    } else if (self.pressingDown) {
+      self.spdY = self.maxSpd;
+    } else {
+      self.spdY = 0;
+    }
+  };
+  Player.list[id] = self;
+  return self;
+};
+Player.list = {};
+
+Player.onConnect = (socket) => {
+  //   console.log("Player connected! " + socket.id);
+
+  const player = Player(socket.id);
   socket.on("keyPress", (data) => {
+    // console.log("keyPress", data.inputId, data.state);
     if (data.inputId === "left") {
       player.pressingLeft = data.state;
     } else if (data.inputId === "right") {
@@ -63,19 +83,39 @@ io.sockets.on("connection", (socket) => {
       player.pressingDown = data.state;
     }
   });
-});
-
-setInterval(() => {
+};
+Player.onDisconnect = (socket) => {
+  delete Player.list[socket.id];
+};
+Player.update = () => {
   const pack = [];
-  for (let i in PLAYER_LIST) {
-    let player = PLAYER_LIST[i];
-    player.updatePosition();
+  for (let i in Player.list) {
+    let player = Player.list[i];
+    player.update();
     pack.push({
       x: player.x,
       y: player.y,
       number: player.number,
     });
   }
+  return pack;
+};
+
+const io = require("socket.io")(serv, {});
+io.sockets.on("connection", (socket) => {
+  socket.id = Math.random();
+  SOCKET_LIST[socket.id] = socket;
+
+  Player.onConnect(socket);
+
+  socket.on("disconnect", () => {
+    delete SOCKET_LIST[socket.id];
+    Player.onDisconnect(socket);
+  });
+});
+
+setInterval(() => {
+  const pack = Player.update();
   for (let i in SOCKET_LIST) {
     let socket = SOCKET_LIST[i];
     socket.emit("newPositions", pack);
