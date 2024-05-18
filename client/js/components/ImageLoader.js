05e4ef04eb525f2c986726ea5fc9e73d5cd32d11
SAY10s <nikodemmajzner@gmail.com>
2024-05-18 15:29:01 +0200

    Moved imgs to separate folders

--- client/js/components/ImageLoader.js
+++ client/js/components/ImageLoader.js
@@ -2,17 +2,17 @@ export class ImageLoader {
   static loadImages() {
     const Img = {};
     const imagePaths = {
-      player: "/client/img/isaac.png",
-      playerShooting: "/client/img/isaacShooting.png",
-      enemy: "/client/img/enemy.png",
-      enemyShooting: "/client/img/enemyShooting.png",
+      player: "/client/img/playerModels/isaac.png",
+      playerShooting: "/client/img/playerModels/isaacShooting.png",
+      enemy: "/client/img/playerModels/enemy.png",
+      enemyShooting: "/client/img/playerModels/enemyShooting.png",
       playerTear: "/client/img/playerTear.png",
       enemyTear: "/client/img/enemyTear.png",
       map: "/client/img/bg.png",
-      soulFullHeart: "/client/img/soulFullHeart.png",
-      soulHalfHeart: "/client/img/soulHalfHeart.png",
-      redFullHeart: "/client/img/redFullHeart.png",
-      redHalfHeart: "/client/img/redHalfHeart.png",
+      soulFullHeart: "/client/img/hearts/soulFullHeart.png",
+      soulHalfHeart: "/client/img/hearts/soulHalfHeart.png",
+      redFullHeart: "/client/img/hearts/redFullHeart.png",
+      redHalfHeart: "/client/img/hearts/redHalfHeart.png",
     };
     for (let key in imagePaths) {
       Img[key] = ImageLoader.loadImage(imagePaths[key]);
